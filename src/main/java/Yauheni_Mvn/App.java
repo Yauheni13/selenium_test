9a14355507af46a8875e8e18d8b68f78848050e8
Yauheni <yauheni.butsko@gmail.com>
2016-10-06 22:26:35 -0400

    initial import

---
+++ src/main/java/Yauheni_Mvn/App.java
@@ -0,0 +1,13 @@
+package Yauheni_Mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
